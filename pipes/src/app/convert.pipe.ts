46d9e8886fc2b97fa01ce339f6f265024fa6bf89
Alex Perea <ing.alexperea@gmail.com>
2023-12-03 12:29:36 -0500

    65 pipes custom arguments

--- pipes/src/app/convert.pipe.ts
+++ pipes/src/app/convert.pipe.ts
@@ -5,12 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
+  transform(value: any, targetUnit: string): any {
     if (!value) {
       return '';
     }
     
-    return value * 1.60934;
+    switch (targetUnit) {
+      case 'km':
+        return value * 1.60934;
+        break;
+
+      case 'm':
+        return value * 1.60934 * 1000;
+        break;
+    }
+    
   }
 
 }
