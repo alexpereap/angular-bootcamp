4438bc9cf9bd768e81ab7cce886f61aa8ae57776
Alex Perea <ing.alexperea@gmail.com>
2023-12-03 08:37:34 -0500

    typing 51 conditional display

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -8,10 +8,37 @@ import { faker } from '@faker-js/faker';
 })
 export class AppComponent {
   randomText = faker.lorem.sentence({ min: 3, max: 5 });
+  randomTextObj : Array<any> = [];
   showSuccess = false;
+
+  constructor() {
+    for (let letter in this.randomText.split("")) {
+      this.randomTextObj.push(
+        {
+          'letter': this.randomText[letter],
+          'class': 'txt-black'
+        }
+      )
+    }
+  }
 
   onInputChange(event: Event) {
     const target = event.target as HTMLInputElement;
+    const inputValue = target.value;
+
+    if (inputValue.length <= this.randomText.length) {
+      for (let key in inputValue.split('')) {
+
+        this.randomTextObj[key].class = inputValue[key] == this.randomTextObj[key].letter
+          ? this.randomTextObj[key].class = 'txt-green'
+          : this.randomTextObj[key].class = 'txt-red';
+      }
+
+      for (let i = inputValue.length; i < this.randomText.length; i++ ){
+        this.randomTextObj[i].class = 'txt-black';
+      }
+    }
+
     this.showSuccess = target.value == this.randomText;
   }
 }
