a1622f8357d29fea0ba9be73b8032b7d1e37bc7b
Alex Perea <ing.alexperea@gmail.com>
2023-12-03 11:33:04 -0500

    pipes 56 pipes in action

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.name = target.value;
+  }
+  
 }
