f78d81017f1b10c0c7a0f8b8ec6fb02f0344be04
Alex Perea <ing.alexperea@gmail.com>
2024-01-12 19:20:08 -0500

    117 ng content with selects

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { SegmentComponent } from './elements/segment/segment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
-    SegmentComponent
   ],
   imports: [
     BrowserModule,
