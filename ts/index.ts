808dd79648eb9e82c61b1b28d007080e0cf1957a
Alex Perea <ing.alexperea@gmail.com>
2024-02-17 08:27:18 -0500

    ts objects, types, anotations

---
+++ ts/index.ts
@@ -0,0 +1,9 @@
+const myName: string = 'Alex';
+
+// objects
+const post :{ title: string, daysOld: number, published: boolean } = {
+    title: 'The title',
+    daysOld: 10,
+    published: true
+};
+
