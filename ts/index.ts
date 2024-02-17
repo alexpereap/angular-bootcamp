b0688b4b92a5ac8ed5023c4dbd9db6f19e8a9e21
Alex Perea <ing.alexperea@gmail.com>
2024-02-17 08:54:59 -0500

    TS 157 classes and properties

--- ts/index.ts
+++ ts/index.ts
@@ -17,4 +17,22 @@ const printPost = (postToPrint: Post) => {
     return `${postToPrint.title} (${postToPrint.daysOld})`;
 }
 
-printPost(post);
+console.log(printPost(post));
+
+class Car {
+    color: string;
+    year: number;
+
+    constructor(color: string, year: number) {
+        this.color = color;
+        this.year = year;
+    }
+
+    drive() {
+        console.log('Vroom');
+    }
+}
+
+const myCar = new Car('red', 2000);
+myCar.drive();
+console.log(myCar.color, myCar.year);
