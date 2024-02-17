56d91cd769134c4638601e2c0d29c76fcaa9624d
Alex Perea <ing.alexperea@gmail.com>
2024-02-17 10:13:05 -0500

    166 function generics

--- ts/generics.ts
+++ ts/generics.ts
@@ -4,3 +4,11 @@ class ValueHolder<TypeForValueProperty> {
 
 const numberHolder = new ValueHolder<number>();
 numberHolder.value; // TS knows now that the value prop is of type number
+
+// function generics
+
+const valueWrapper = <T>(value: T): T[] => {
+    return [value];
+}
+
+valueWrapper<number>(10);
