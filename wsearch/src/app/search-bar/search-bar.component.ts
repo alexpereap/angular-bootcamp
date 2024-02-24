232f004edd658d89708167762c2b2b7f5542f480
Alex Perea <ing.alexperea@gmail.com>
2024-02-24 13:11:10 -0500

    170 component design methodology

--- wsearch/src/app/search-bar/search-bar.component.ts
+++ wsearch/src/app/search-bar/search-bar.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
+  term = '';
 
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.term = target.value;
+  }
 }
