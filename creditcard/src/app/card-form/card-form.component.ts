074b50f715ee36f43a2740f17d3a5e5f9b9dd9c3
Alex Perea <ing.alexperea@gmail.com>
2024-03-07 16:52:53 -0500

    added validation messages to reusable component

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -10,7 +10,8 @@ export class CardFormComponent {
   cardForm = new FormGroup({
     name: new FormControl('', [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      Validators.maxLength(5),
     ])
   })
 
